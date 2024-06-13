2549b18830b711aaaf13e2ce3cfe3802e56189e6
Moris Mwai <morismwai1@gmail.com>
2024-06-13 18:48:55 +0300

    Update experience section with moving border component

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
-import { LampContainer, LampDemo } from './ui/Lamp'
+import { LampDemo } from './ui/Lamp'
+import { Button } from './ui/MovingBorder'
+import { workExperience } from '@/data'
 
 const Experience = () => {
   return (
     <div className='py-20' id='certifications'>
+      <div className='-top-20 h-[55vh] max-lg:-mt-5 overflow-hidden'>
         <LampDemo />
+      </div>
+        
 
-        {/* Certification cards */}
-        <div className='flex flex-col items-center max-lg:mt-10'></div>
+        {/* Experience cards */}
+        <div className='w-full -mt-20 md:-mt-20 grid lg:grid-cols-4 grid-cols-1 gap-10 max-lg:-mt-5'>
+          {workExperience.map((card) => (
+            <Button
+            key={card.id}
+            duration={Math.floor(Math.random() * 10000) + 10000}
+            borderRadius='1.75rem'
+            className='flex-1 text-white border-neutral-200 dark:border-slate-800'>
+              <div className='flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2'>
+                <img src={card.thumbnail} alt={card.thumbnail} className='lg:w-32 md:w-20 w-16' />
+                <div className='lg:ms-5'>
+                  <h1 className='text-start text-xl md:text-2xl font-bold'>
+                    {card.title}
+                  </h1>
+                  <p className='text-start text-white-100 mt-3 font-semibold'>
+                    {card.desc}
+                  </p>
+                </div>
+              </div>
+            </Button>
+          ))}
+        </div>
     </div>
   )
 }
--- components/ui/Lamp.tsx
+++ components/ui/Lamp.tsx
@@ -16,7 +16,7 @@ export function LampDemo() {
         }}
         className="mt-5 bg-gradient-to-br from-slate-300 to-slate-800 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl heading"
       >
-        My 
+        <span className="text-white">My</span> 
         <span className="text-purple"> Work Experience</span>
       </motion.h1>
     </LampContainer>
@@ -70,9 +70,9 @@ export const LampContainer = ({
           <div className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
           <div className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
         </motion.div>
-        <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
-        <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
-        <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-cyan-500 opacity-50 blur-3xl"></div>
+        <div className="absolute w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
+        <div className="absolute z-50 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
+        <div className="absolute inset-auto z-50 h-30 w-[28rem] md:w-[15rem] -translate-y-1/2 rounded-full bg-cyan-500 opacity-50 blur-3xl"></div>
         <motion.div
           initial={{ width: "8rem" }}
           whileInView={{ width: "16rem" }}
@@ -81,7 +81,7 @@ export const LampContainer = ({
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl"
+          className="absolute inset-auto z-30 h-36 w-64 md:w-[15rem] -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl"
         ></motion.div>
         <motion.div
           initial={{ width: "15rem" }}
@@ -91,7 +91,7 @@ export const LampContainer = ({
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-cyan-400 "
+          className="absolute inset-auto z-50 h-0.5 w-[30rem] md:w-[15rem] -translate-y-[7rem] bg-cyan-400 "
         ></motion.div>
 
         <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950 "></div>
