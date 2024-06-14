fd6b9e1e5631bba4a2e0856eee66d7e56ae090b4
Moris Mwai <morismwai1@gmail.com>
2024-06-14 20:07:23 +0300

    Adjust styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import { socialMedia } from '@/data'
 
 const Footer = () => {
   return (
-    <footer className='w-full pb-10 mb-[100px] md:mb-5' id='contact'>
+    <footer className='w-full pb-10 mb-[40px] md:mb-7' id='contact'>
         <div className='w-full absolute left-0 -bottom-72 min-h-96'>
             <img 
                 src="/footer-grid.svg" 
@@ -15,7 +15,7 @@ const Footer = () => {
              />
         </div>
 
-        <div className='flex flex-col items-center top-[25%] translate-y-[-25%]'>
+        <div className='flex flex-col items-center top-[30%] translate-y-[-30%]'>
             <h1 className='heading lg:max-w-[45vw]'>
                 Ready to Elevate <span className='text-purple'>Your Processes</span> with Intelligent Automation?
             </h1>
@@ -29,10 +29,10 @@ const Footer = () => {
             </a>
         </div>
 
-        <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
+        <div className='flex mt-13 md:flex-row flex-col justify-between items-center'>
             <p className='md:text-base text-sm md:font-normal font-light'>Copyright © 2024 Moris</p>
 
-            <div className='flex items-center md:gap-3 gap-6'>
+            <div className='flex items-center md:gap-3 gap-6 mt-2'>
                 {socialMedia.map((profile) => (
                     <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
                         <img src={profile.img} alt={profile.id} width={20} height={20} />
